2c256a65986988a6027d77159780197006ec20ab
Marijn Jongsma <99289232+Marijn03@users.noreply.github.com>
2024-06-22 00:18:59 +0200

    added recent code of bash file

--- llama3.sh
+++ llama3.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-# SBATCH --time=5:00:00
-# SBATCH --partition=gpu
-# SBATCH --gpus-per-node=a100:1
-# SBATCH --job-name=llms
-# SBATCH --mem=512G
+#SBATCH --time=0:10:00
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --job-name=phi3_llm
+#SBATCH --mem=50G
 
 source $HOME/venvs/first_env/bin/activate
+
 module purge
 module load Python/3.9.6-GCCcore-11.2.0
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
-module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0 
+module load PyTorch/2.1.2-foss-2022b
+module list
 
-python3 --version
-which python
+# python3 --version
+# which python
 
-python3 -m pip install transformers torch langchain accelerate --no-cache-dir
+# python3 -m pip install transformers torch langchain accelerate evaluate bert_score
+# python3 -m pip install protobuf==3.20.*
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
-python3 llama3.py
+# python3 -m pip install huggingface_hub
+# Note: You should enter your personal huggingface authenticator here
+python3 -m -c "from huggingface_hub.hf_api import HfFolder; HfFolder.save_token('PERSONAL_AUT')"
+
+# python3 llama3_develop.py
+# python3 llama3_evaluation.py
+python3 get_scores.py
